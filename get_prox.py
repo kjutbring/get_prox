da8a3992442159b6b8cb33a39eaeb8a92ddd5a04
kjutbring <kittyj@cmail.nu>
2015-02-08 02:26:33 +0300

    initial commit

---
+++ get_prox.py
@@ -0,0 +1,30 @@
+#!venv/bin/python
+
+from bs4 import BeautifulSoup
+import mechanize
+
+in_url = 'http://proxy-list.org/english/index.php?p='
+
+def get_proxies(url):
+	br = mechanize.Browser()
+	response = br.open(url)
+
+	hot_soup = BeautifulSoup(response)
+
+	div_content = hot_soup.find("div", {"class": "table"})
+
+	proxies = div_content.findAll("li", {"class": "proxy"})
+
+	return proxies
+
+def main():
+	proxlist = []
+
+	for i in range(10):
+		proxlist.append(get_proxies(in_url+str(i+1)))
+
+	for a in proxlist:
+		print a
+
+if __name__ == "__main__":
+	main()
