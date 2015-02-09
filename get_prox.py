fb599bc670af04152b5a52756ad729d5f59437fe
kjutbring <kittyjutbring@countermail.com>
2015-02-10 00:25:39 +0300

    use requests over mechanize

--- get_prox.py
+++ get_prox.py
@@ -10,25 +10,25 @@
 ##################################################
 
 from bs4 import BeautifulSoup
-import mechanize
+import os
 import re
+import requests
 
 in_url = 'http://proxy-list.org/english/index.php?p='
 
 def get_proxies(url):
-	br = mechanize.Browser()
-
+	
 	print '[+] Starting...'
 	
 	# add proxies to list
 	proxy_list = []
 	for i in range(10):
 		try:
-			resp = br.open(url+str(i+1))
+			resp = requests.get(url+str(i+1))
 		except:
 			print '[-] Failed, check your internet connection.'
 
-		cold_soup = BeautifulSoup(resp)
+		cold_soup = BeautifulSoup(resp.text)
 		div = cold_soup.find("div", {"class": "table"})
 		prox = div.findAll("li", {"class": "proxy"})
 
@@ -38,7 +38,7 @@ def get_proxies(url):
 
 	return proxy_list
 
-def test_proxies():
+def test_proxies(proxies):
 	pass
 
 def main():
