9fb277a4a4ff699de0eefe748e976a31f6573dc5
Spaddex <Spaddex@swehack.org>
2016-09-29 21:03:43 +0200

    Added MultiThreading
    Added mutlithreading. Doesn't work perfectly though, but heck of a lot faster at least.

--- get_prox.py
+++ get_prox.py
@@ -14,23 +14,24 @@ from bs4 import BeautifulSoup
 
 import re
 import mechanize
-import time
 import threading
+
+
+
 
 in_url = 'http://proxy-list.org/english/index.php?p='
 
+
 def get_proxies(url):
-
 	print '[+] Starting...'
-
 	br = mechanize.Browser()
 	br.set_handle_robots(False)
 
 	# add proxies to list
 	proxy_list = []
-	for i in range(3):
+	for i in range(10):
 		try:
-			resp = br.open(url+str(i+1))
+			resp = br.open(url + str(i + 1))
 		except:
 			print '[-] Failed, check your internet connection.'
 
@@ -40,45 +41,52 @@ def get_proxies(url):
 
 		# extraction from html string
 		for proxy in prox:
-			slice1=proxy.text[7:]
-			slice2=slice1[:-2]
+			slice1 = proxy.text[7:]
+			slice2 = slice1[:-2]
 			proxy_list.append(slice2.decode('base64'))
 	print "Got " + str(len(proxy_list)) + " Proxies"
 	return proxy_list
 
-def test_proxies(proxies):
+
+def ProxyTester(SingleProxy):
 	br = mechanize.Browser()
 	br.set_handle_robots(False)
 	br.addheaders = [('User-agent', 'Firefox')]
+	with open('outprox.txt', 'a') as out_file:
+		try:
+			print ('\033[33m' + "[+] Testing proxy: " + SingleProxy + '\033[37m')
+			br.set_proxies({"http": SingleProxy})
+			req = br.open("http://icanhazip.com", timeout=30)
+			ipGotten=req.read().strip()
+			br.close()
+			strip_proxy = ''.join(re.findall(r'[0-9]+(?:\.[0-9]+){3}', str(SingleProxy)))
 
-	with open('outprox.txt', 'w') as out_file:
-		for proxy in proxies:
-			try:
-				print ('\033[33m' + "[+] Testing proxy: " + proxy + '\033[37m')
-				br.set_proxies({"http": proxy})
-				now = time.time()
-				br.set_handle_refresh(False)
-				resp = br.open("http://icanhazip.com")
-				latency = (time.time())-now
-				# strip port for comparission
-				strip_proxy = ''.join(re.findall(r'[0-9]+(?:\.[0-9]+){3}', str(proxy)))
+			# Sort though the responses:
+			if ipGotten == strip_proxy:
+					print ('\033[32m' + str(strip_proxy) + " is fine" + '\033[37m')
+					out_file.write(SingleProxy + "\n")
+			else:
+				print ('\033[31m' + "[-] Proxy not ok! (" + str(strip_proxy)  + ') \033[37m')
 
-				if(resp.wrapped.code==200):
-					if latency < 4:
-						print ('\033[32m' + str(strip_proxy) + " is fine" + '\033[37m')
-						out_file.write(proxy + "\n")
-					elif latency > 4:
-						print ('\033[33m' + str(strip_proxy) + " is kind of slow (latency: " + str(int(latency)) + " sec), ignoring." + '\033[37m')
-				else:
-					print ('\033[31m' + str(strip_proxy) + " is no good" + '\033[37m')
+		except Exception as e:
+			print e
+			print ('\033[31m' + "[-] Connection error, proxy: " + SingleProxy + '\033[37m')
 
-			except:
-				print ('\033[31m' + "[-] Connection error, proxy: " + proxy + '\033[37m')
 
 def main():
 	plist = get_proxies(in_url)
+	threadList = []
+	NumberOfThreads = 7
+	for each in range(NumberOfThreads):
+		try:
+			while plist > 0:
+				NewProxy = plist.pop()
+				t = threading.Thread(target=ProxyTester, args=(NewProxy,))
+				t.start()
+				threadList.append(t)
 
-	test_proxies(plist)
+		except Exception as e:
+			print e
 
 if __name__ == "__main__":
 	main()
